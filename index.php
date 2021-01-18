49da6fb02b065deace914aba9ab102e1ac4ba373
alvarosiles <alvarosiles1@gmail.com>
2021-01-18 17:00:06 -0400

    ok

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 <html>
 
 <body>
-
-    <h1>My first PHP page</h1>
-    <a href="https://www.w3schools.com/php/php_syntax.asp"> web PHP</a>
-    <a href="http://localhost/server_php/">localost</a>
-    <?php
+    <a href="http://localhost/server_php/"></a>
+     <?php
     echo "Hello World!";
     ?>
 
-<a href="https://alvarosiles.github.io/home"></a>
+ 
 
 
 </body>
