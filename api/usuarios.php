8fbd89b6e0f31bc3b628b45b8d17ced6f1926e00
alvarosiles <alvarosiles1@gmail.com>
2021-01-24 21:30:25 -0400

    buenisimo

--- api/usuarios.php
+++ api/usuarios.php
@@ -2,25 +2,40 @@
 
 //localost:8080/.../api/usuarios.php
 //echo "Metodo http: " . $_SERVER['REQUEST_METHOD'];
+//echo "informacion: " . file_get_contents('php://input');
+
+header("Content-Type: application/json");
+include_once("../clases/class-usuario.php");
 
 switch ($_SERVER['REQUEST_METHOD']) {
-
     case 'POST':
-        echo "guardar";
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        $usuario = new Usuario($_POST['userId'], $_POST['userName'], $_POST['password'], $_POST['tipo']);
+        $usuario->guardarUsuario();
+        $resultado["mensaje"] = "guardar usuario, informacion:" . json_encode($_POST);
+        echo json_encode($resultado);
         break;
-
     case 'GET':
-        echo "obtener usuario/100";
+        if (isset($_GET['id'])) {
+            Usuario::obtenerUsuario($_GET['id']);
+            //$resultado["mensaje"] = "retornar el usuario con el id:" . $_GET['id'];
+            //echo json_encode($resultado);
+        } else {
+            Usuario::obtenerUsuarios();
+            //$resultado["mensaje"] = "retornar todos los usuarios";
+            //echo json_encode($resultado);
+        }
         break;
-
     case 'PUT':
-        echo "actualizar usuario/1";
+        $_PUT = json_decode(file_get_contents('php://input'), true);
+        $resultado["mensaje"] = "actualizar un usuario con el id: " . $_GET['id'] . ",informacion a actualizar" . json_encode($_PUT);
+        echo json_encode($resultado);
         break;
-
     case 'DELETE':
-        echo "eliminar usuario/1";
+        Usuario::eliminarUsuario($_GET['id']);
+        $resultado["mensaje"] =  "eliminar un usuario con el id: " . $_GET['id'];
+        echo json_encode($resultado);
         break;
-
 }
 
 
@@ -33,3 +48,36 @@ switch ($_SERVER['REQUEST_METHOD']) {
 //put
 //eliminar un usuario
 //delete
+
+
+
+
+
+/*
+api
+interface proramacion apliacion
+
+
+formato simple y estructurado en 
+json formato
+xsml formato
+
+
+arquitectectura
+rest api
+
+representacional state transfer
+tranferia de estado representacional
+
+caracteristicas
+protocolo lciente - servidor
+consjunto de operaciones bien definidas
+sintaxis universal para identificar recursos (uri)
+
+operaciones - para metodos http
+crear c - post   
+leer r - get
+actulizar u - put
+eliminar d - delete
+
+*/
--- clases/class-usuario.php
+++ clases/class-usuario.php
@@ -8,8 +8,9 @@ class Usuario
     private $password;
     private $tipo;
 
-    public function __construct($userName, $password, $tipo)
+    public function __construct($userId,$userName, $password, $tipo)
     {
+        $this->userId = $userId;
         $this->userName = $userName;
         $this->password = $password;
         $this->tipo = $tipo;
@@ -44,7 +45,6 @@ class Usuario
         return $this->password = $password;
     }
 
-
     public function getTipo()
     {
         return $this->tipo;
@@ -53,5 +53,69 @@ class Usuario
     public function setTipo($tipo)
     {
         return $this->tipo = $tipo;
+    }
+
+    public function __toString()
+    {
+        return $this->userId . " " . $this->userName . " " . $this->password . " " . "(" . $this->tipo . ")";
+    }
+
+
+    public function guardarUsuario()
+    {
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        $usuarios = json_decode($contenidoArchivo, true);
+        $usuarios[] = array(
+            "userId" => $this->userId,
+            "userName" => $this->userName,
+            "password" => $this->password,
+            "tipo" => $this->tipo
+        );
+        $archivo = fopen("../data/usuarios.json", "w");
+        fwrite($archivo, json_encode($usuarios));
+        fclose($archivo);
+    }
+
+
+    public static function obtenerUsuarios()
+    {
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        echo $contenidoArchivo;
+    }
+
+    public static function obtenerUsuario($indice)
+    {
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        $usuarios = json_decode($contenidoArchivo, true);
+        echo json_encode($usuarios[$indice]);
+    }
+
+
+
+    public function actualizarUsuario($indice)
+    {
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        $usuarios = json_decode($contenidoArchivo, true);
+
+        $usuario = array(
+            'userId' => $this->userId,
+            'userName' => $this->userName,
+            'password' => $this->password,
+            'tipo' => $this->tipo
+        );
+
+        $usuarios[$indice] = $usuario;
+        $archivo = fopen('../data/usuarios.json', 'w');
+        fwrite($archivo, json_encode($usuarios));
+        fclose($archivo);
+    }
+    public static function eliminarUsuario($indice)
+    {
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        $usuarios = json_decode($contenidoArchivo, true);
+        array_splice($usuarios, $indice, 1);
+        $archivo = fopen('../data/usuarios.json', 'w');
+        fwrite($archivo, json_encode($usuarios));
+        fclose($archivo);
     }
 }
